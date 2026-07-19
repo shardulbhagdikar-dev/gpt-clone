c7e894eda2b16f21e5dc9becd7f249efe119e639
Shardul Bhagdikar <shardul.bhagdikar@gmail.com>
2026-07-19 08:45:24 +0530

    Set up Authentication with clerk Created User model Onboarding infor is now stored in DB.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <ModeToggle/>
+      <UserButton/>
       </div>
   );
 }
